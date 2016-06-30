a7e15e991aaad45e33b0fe37eed48f8210502534
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2016-06-30 14:30:21 -0400

    Add some routing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,21 @@
 
-var app = angular.module("app", ['ngRoute']);
-  app.config(function($routeProvider){
+console.log("JS connected");
+
+var mainApp = angular.module("mainApp", ['ngRoute']);
+  mainApp.config(function($routeProvider) {
     $routeProvider
     .when('/home', {
-      templateUrl: 'product_grid.html',
+      templateUrl: '../../product_grid.html'
     })//end of /home routing
+
+    .when('/product_detail', {
+      templateUrl: '../../product_detail.html'
+    })//end of /product_detail routing
 
     .otherwise({
 
-    redirectTo: '/home',
+    redirectTo: '/home'
     });
   }); //end of consApp routing
+
+console.log("JS makes to end");
